68572bec7931bae1284ea3aa9f8576f1f6f6c392
TheLeerName <lesha.nechaev.05@mail.ru>
2022-01-06 01:55:30 +0700

    fix of randomly placed notes and another things
    1. Fix of randomly placed notes (uses in "upperkeying" maps)
    1. New traces and warns
    2. StateWorker deleted and added Options
    3. Sync with tposejank's Extra Keys mod
    4. Fix crash with event note

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Mania Converter
-Converter of FNF maps/charts, example from 7 key to 4 key
+Converter of FNF maps/charts, example from 4 key to 6 key.
 
 ## How to use this?
 1. Run a EXE file, it will be crash.
 2. Open `options.json` file:
-- Enter the number of keys in `to_key` you want to convert to.
+- `to_key` for a number of keys you want to convert to.
+- `from_key_default` for default number of keys, if in chart/map `mania` line is not exist.
+- `to_key_default` for default number of keys, if you type value in `to_key` less than 1 or more than length of array in algorithm.
+- `algorithm` for placing a notes.
+- `extra_keys_sync` for sync with [tposejank's Extra Keys Mod](https://gamebanana.com/mods/333373).
 - `from_file` for name of chart/map before converting.
 - `to_file` for name of chart/map after converting.
 3. Move your chart/map to the converter folder.
 4. Run a EXE file again.
 5. Now you have a converted chart/map!
+
+> I dont want do the ui of options in app, please no :(
--- source/TitleState.hx
+++ source/TitleState.hx
@@ -1,17 +1,12 @@
 package;
 
+import flixel.addons.ui.FlxUIState;
+import flixel.addons.transition.FlxTransitionableState;
 import lime.app.Application;
 
 using StringTools;
 
-typedef Options = {
-	var from_file:String;
-	var to_file:String;
-	var to_key:Int;
-	var algorithm:Array<Dynamic>;
-}
-
-class TitleState extends StateWorker
+class TitleState extends FlxUIState
 {
 	/*function checkFolders()
 	{
@@ -29,142 +24,13 @@ class TitleState extends StateWorker
 	override function create()
 	{
 		//checkFolders();
+		FlxTransitionableState.skipNextTransOut = true;
+		FlxTransitionableState.skipNextTransIn = true;
 
 		Debug.log.trace('Starting... (version ${Application.current.meta.get('version')})');
 
-		var options:Options = {
-			from_file: "beatmap.json",
-			to_file: "beatmap-converted.json",
-			to_key: 4,
-			algorithm: [
-					[],
-					[
-						[],
-						[[0,1],   [2,3]],
-						[[0,1,2],   [3,4,5]],
-						[[0,1,2,3],   [4,5,6,7]],
-						[[0,1,2,3,4],   [5,6,7,8,9]],
-						[[0,1,2,3,4,5],   [6,7,8,9,10,11]],
-						[[0,1,2,3,4,5,6],   [7,8,9,10,11,12,13]],
-						[[0,1,2,3,4,5,6,7],   [8,9,10,11,12,13,14,15]],
-						[[0,1,2,3,4,5,6,7,8],   [9,10,11,12,13,14,15,16,17]]
-					],
-					[
-						[],
-						[0, 0,   1, 1],
-						[[0,1], 2,   [3,4], 5],
-						[[0,1], [2,3],   [4,5], [6,7]],
-						[[0,1,2], [3,4],   [5,6,7], [8,9]],
-						[[0,1,2], [3,4,5],   [6,7,8], [9,10,11]],
-						[[0,1,2,3], [4,5,6],  [7,8,9,10], [11,12,13]],
-						[[0,1,2,3], [4,5,6,7],    [8,9,10,11], [12,13,14,15]],
-						[[0,1,2,3,4], [5,6,7,8],   [9,10,11,12,13], [14,15,16,17]]
-					],
-					[
-						[],
-						[0, 0, 0,   1, 1, 1],
-						[0, 0, 1,   2, 2, 3],
-			
-						[[0,1], 2, 3,   [4,5], 6, 7],
-						[[0,1], 2, [3,4],   [5,6], 7, [8,9]],
-						[[0,1], [2,3], [4,5],   [6,7], [8,9], [10,11]],
-						[[0,1], [2,3,4],[5,6],   [7,8], [9,10,11], [12,13]],
-						[[0,1,2], [3,4], [5,6,7],    [8,9,10], [11,12], [13,14,15]],
-						[[0,1,2], [3,4,5], [6,7,8],   [9,10,11], [12,13,14], [15,16,17]]
-					],
-					[
-						[],
-						[0, 0, 0, 0,   1, 1, 1, 1],
-						[0, 0, 1, 1,   2, 2, 3, 3],
-						[0, 0, 1, 2,   3, 3, 4, 5],
-			
-						[0, 1, [2,3], 4,   5, 6, [7,8], 9],
-						[[0,3], 4, 1, [2,5],   [6,9], 10, 7, [8,11]],
-						[[0,4], 5, [1,3], [2,6],   [7,11], 12, 10, [9,13]],
-						[[0,4], [1,5], [2,6], [3,7],    [8,12], [9,13], [10,14], [11,15]],
-						[[0,5], [1,6], [2,4,7], [3,8],   [9,14], [10,15], [11,13,16], [12,17]]
-					],
-					[
-						[],
-						[0, 0, 0, 0, 0,   1, 1, 1, 1, 1],
-						[0, 0, 0, 1, 1,   2, 2, 2, 3, 3],
-						[0, 0, 1, 2, 2,   3, 3, 4, 5, 5],
-						[0, 1, 2, 2, 3,   4, 5, 6, 6, 7],
-			
-						[3, 4, 1, 0, [2,5],   9, 10, 7, 6, [8,11]],
-						[[0,4], 5, 1, 3, [2,6],   [7,11], 12, 8, 10, [9,13]],
-						[[0,4], [1,5], 2, 6, [3,7],    [8,12], [9,13], 10, 14, [11,15]],
-						[[0,5], [1,6], 4, [2,7], [3,8],   [9,14], [10,15], 13, [11,16], [12,17]]
-					],
-					[
-						[],
-						[0, 0, 0, 0, 0, 0,   1, 1, 1, 1, 1, 1],
-						[0, 0, 0, 1, 1, 1,   2, 2, 2, 3, 3, 3],
-						[0, 0, 1, 1, 2, 2,   3, 3, 4, 4, 5, 5],
-						[0, 2, 3, 0, 1, 3,   4, 6, 7, 4, 5, 7],
-						[3, 2, 4, 0, 1, 4,   8, 7, 9, 5, 6, 9],
-			
-						[0, [1,3], 2, 4, 5, 6,   7, [8,10], 9, 11, 12, 13],
-						[0, [1,5], 3, 4, [2,6], 7,   8, [9,13], 11, 12, [10,14], 15],
-						[0, [1,6], 3, 5, [2,4,7], 8,   9, [10,15], 12, 14, [11,13,16], 17]
-					],
-					[
-						[],
-						[0, 0, 0, 0, 0, 0, 0, 0,   1, 1, 1, 1, 1, 1, 1, 1],
-						[0, 0, 0, 0, 1, 1, 1, 1,   2, 2, 2, 2, 3, 3, 3, 3],
-						[0, 0, 0, 1, 1, 2, 2, 2,   3, 3, 3, 4, 4, 5, 5, 5],
-						[0, 1, 2, 3, 0, 1, 2, 3,   4, 5, 6, 7, 4, 5, 6, 7],
-						[0, 1, 2, 4, 0, 1, 3, 4,   5, 6, 7, 9, 5, 6, 8, 9],
-						[0, 1, 4, 2, 3, 1, 4, 5,   6, 7, 10, 8, 9, 7, 10, 11],
-						[0, 1, 4, 2, 3, 1, 5, 6,   7, 8, 11, 9, 10, 8, 12, 13],
-			
-						[0, [1,5], 3, 4, 2, 1, 7,   8, [9,13], 11, 12, 10, 14, 15],
-						[0, [1,6], 3, 4, 5, [2,7], 8,   9, [10,15], 12, 13, 14, [11,16], 17]
-					],
-					[
-						[],
-						[0, 0, 0, 0, 0, 0, 0, 0,   1, 1, 1, 1, 1, 1, 1, 1],
-						[0, 0, 0, 0, 1, 1, 1, 1,   2, 2, 2, 2, 3, 3, 3, 3],
-						[0, 0, 0, 1, 1, 2, 2, 2,   3, 3, 3, 4, 4, 5, 5, 5],
-						[0, 1, 2, 3, 0, 1, 2, 3,   4, 5, 6, 7, 4, 5, 6, 7],
-						[0, 1, 2, 4, 0, 1, 3, 4,   5, 6, 7, 9, 5, 6, 8, 9],
-						[0, 1, 4, 2, 3, 1, 4, 5,   6, 7, 10, 8, 9, 7, 10, 11],
-						[0, 1, 4, 2, 3, 1, 5, 6,   7, 8, 11, 9, 10, 8, 12, 13],
-			
-						[0, 1, [2,4], 3, 5, 6, 7, 8,   9, 10, [11,13], 12, 14, 15, 16, 17]
-					],
-					[
-						[],
-						[0, 0, 0, 0, 0, 0, 0, 0, 0,   1, 1, 1, 1, 1, 1, 1, 1, 1],
-						[0, 0, 0, 0, 0, 1, 1, 1, 1,   2, 2, 2, 2, 2, 3, 3, 3, 3],
-						[0, 0, 0, 1, 1, 1, 2, 2, 2,   3, 3, 3, 4, 4, 4, 5, 5, 5],
-						[0, 1, 2, 3, 2, 0, 1, 2, 3,   4, 5, 6, 7, 6, 4, 5, 6, 7],
-						[0, 1, 3, 4, 2, 0, 1, 3, 4,   5, 6, 8, 9, 7, 5, 6, 8, 9],
-						[0, 1, 4, 2, 4, 3, 1, 4, 5,   6, 7, 10, 8, 10, 9, 7, 10, 11],
-						[0, 1, 5, 2, 3, 4, 1, 5, 6,   7, 8, 12, 9, 10, 11, 8, 12, 13],
-						[0, 1, 2, 3, 2, 4, 5, 6, 7,   8, 9, 10, 11, 10, 12, 13, 14, 15]
-					]
-				]
-		}
-
-		if (!FileAPI.file.exists('options.json'))
-		{
-			Debug.log.warn('Path options.json not found! Creating a one...');
-			//return;
-			FileAPI.file.saveFile('options.json', FileAPI.file.stringify(options, "\t", false));
-		}
-		var json:Dynamic = FileAPI.file.parseJSON('options.json');
-		if (json.to_key == null || json.algorithm == null || json.from_file == null || json.to_file == null)
-			FileAPI.file.saveFile('options.json', FileAPI.file.stringify(options, "\t", false));
-		options = json;
-
+		var options:Options.OptionsJSON = Options.get.options();
 		var alg:Array<Dynamic> = options.algorithm;
-		Debug.log.trace('Randoming notes in algorithm...');
-		for (i1 in 0...alg.length)
-			for (i2 in 0...alg[i1].length)
-				for (i3 in 0...alg[i1][i2].length)
-					if (alg[i1][i2][i3].length > 1)
-						alg[i1][i2][i3] = alg[i1][i2][i3][Std.random(alg[i1][i2][i3].length)];
 
 		if (!FileAPI.file.exists(options.from_file))
 		{
@@ -174,30 +40,52 @@ class TitleState extends StateWorker
 		}
 		var json:Dynamic = FileAPI.file.parseJSON(options.from_file);
 
-		var from_key:Int = json.song.mania;
+		var from_key:Dynamic = 0;
+		if (json.song.mania == null)
+			from_key = options.from_key_default;
+		else
+			from_key = json.song.mania + (options.extra_keys_sync ? 1 : 0);
 
-		var to_key:Int = options.to_key;
-		if (to_key < 1 || to_key > 9)
-			to_key = 4;
+		var to_key:Int = 0;
+		if (options.to_key < 1 || options.to_key > options.algorithm.length - 1)
+		{
+			Debug.log.warn('Key count ${options.to_key < 1 ? 'less than 1' : 'more than ${options.algorithm.length - 1}'}, returning ${options.to_key_default}...');
+			to_key = options.to_key_default;
+		}
+		else
+			to_key = options.to_key;
+
+		if (from_key == to_key)
+		{
+			Debug.log.error('Chart/Map already is ${from_key} key!');
+			Application.current.window.close();
+			return;
+		}
 
 		Debug.log.trace('Converting notes...');
-		for (i in 0...json.song.notes.length) for (i1 in 0...json.song.notes[i].sectionNotes.length)
-			json.song.notes[i].sectionNotes[i1][1] = alg[from_key][to_key][json.song.notes[i].sectionNotes[i1][1]];
-
-		var check:Array<Dynamic> = [[], []];
-		for (i in 0...json.song.notes.length) for (i1 in 0...json.song.notes[i].sectionNotes.length)
+		for (i in 0...json.song.notes.length)
+			for (i1 in 0...json.song.notes[i].sectionNotes.length)
 			{
-				check[0].push(json.song.notes[i].sectionNotes[i1][0]);
-				check[1].push(json.song.notes[i].sectionNotes[i1][1]);
+				if (json.song.notes[i].sectionNotes[i1][1] >= 0)
+				{
+					var alg_var:Dynamic = alg[from_key][to_key][json.song.notes[i].sectionNotes[i1][1]];
+					if (alg_var.length > 1)
+						json.song.notes[i].sectionNotes[i1][1] = alg_var[Std.random(alg_var.length)];
+					else if (alg_var.length == 1)
+						json.song.notes[i].sectionNotes[i1][1] = alg_var[0];
+					else
+						json.song.notes[i].sectionNotes[i1][1] = alg_var;
+				}
 			}
-		var ex:Array<Dynamic> = [[2, 3, 2], [5, 1, 5]];
-		for (i in 0...ex[0].length)
-			if (ex[0][i])
 
-		json.song.mania = to_key;
+		var value:Float = 0;
+		for (i in 0...json.song.notes.length)
+			value += json.song.notes[i].sectionNotes.length;
+
+		json.song.mania = to_key + (options.extra_keys_sync ? -1 : 0);
 		FileAPI.file.saveFile(options.to_file, FileAPI.file.stringify(json, "\t", false));
 
-		Debug.log.trace('Successfully converted notes of ${json.song.song} from ${from_key} keys to ${to_key} keys!');
+		Debug.log.trace('Successfully converted ${json.song.song} from ${from_key} keys to ${to_key} keys (${value} notes, include events)!');
 		Application.current.window.close();
 
 		super.create();
