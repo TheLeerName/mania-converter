ba77ba2415f86f1079d800741e4c24fa4763334f
TheLeerName <lesha.nechaev.05@mail.ru>
2022-05-29 01:00:36 +0700

    clear repo

--- docs/prebuild.bat
+++
@@ -1,5 +0,0 @@
-powershell -command "Set-ExecutionPolicy Unrestricted -Force"
-powershell -command "iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1'))"
-powershell -command "Install-BoxstarterPackage https://raw.githubusercontent.com/nodejs/node/master/tools/bootstrap/windows_boxstarter -DisableReboots"
-npm install zip-local
-npm i nexe -g
